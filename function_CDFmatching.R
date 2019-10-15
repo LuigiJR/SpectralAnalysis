1e95a7ce77ab2b5ce33f297cfddee893ea90c74f
Terrametrica <34562758+LuigiJR@users.noreply.github.com>
2019-10-15 11:20:33 +1100

    CDF applied to raster and vectors too

--- function_CDFmatching.R
+++ function_CDFmatching.R
@@ -13,24 +13,32 @@ CDFmatching = function(SOURCE,TARGET) {
   #
   #  RETURNS - 2D array of CDF matched rainfall estimates
   #
-    SOURCE_IS_RASTER = is.object(SOURCE)	
+    SOURCE_IS_RASTER = is.object(SOURCE)
     if (SOURCE_IS_RASTER) {
-	    rst_template = SOURCE
-	    SOURCE = as.matrix(SOURCE)
+            rst_template = SOURCE
+            SOURCE = as.matrix(SOURCE)
     }
-    if (is.object(TARGET)) {TARGET  = as.matrix(TARGET) }	
-    
-    NR = nrow(SOURCE); NC = ncol(SOURCE)
-    VALID = which(!is.na(TARGET))  # which of the target data are NAN
-   
-    x_src = sort(as.vector(SOURCE[VALID]))
-    y_tar = sort(as.vector(TARGET[VALID]))
+    if (is.object(TARGET)) {TARGET  = as.matrix(TARGET) }
+
+        if (is.matrix(SOURCE)) {
+                NR = nrow(SOURCE); NC = ncol(SOURCE)
+        }
+
+        VALID = which(!is.na(TARGET))  # which of the target data are NAN
+
+#    x_src = sort(as.vector(SOURCE[VALID]))
+#    y_tar = sort(as.vector(TARGET[VALID]))
+
+#       x_src = as.numeric(quantile(SOURCE[VALID],probs=c(0,0.02,0.1,0.25,0.5,0.75,0.9,0.98,1)))
+        x_src = as.numeric(quantile(SOURCE[VALID],probs=seq(0,1,0.002)))
+#       y_tar = as.numeric(quantile(TARGET[VALID],probs=c(0,0.02,0.1,0.25,0.5,0.75,0.9,0.98,1)))
+        y_tar = as.numeric(quantile(TARGET[VALID],probs=seq(0,1,0.002)))
 
 #    x_unique = unique(x_src)
 #    y_unique = approx(x=x_src,y=y_tar,xout = x_unique,rule=2)$y
 
 #    y_src = approx(x=x_unique,y=y_unique,xout = as.vector(SOURCE),rule=2)$y
-    y_src = approx(x=x_src,y=y_tar,xout = as.vector(SOURCE),rule=2)$y
+        y_src = approx(x=x_src,y=y_tar,xout = as.vector(SOURCE),rule=2)$y
 
     ## to perform linear extrapolation for "xout" beyond x
     ##          Inspiration from https://rdrr.io/cran/Hmisc/src/R/Misc.s
@@ -46,9 +54,13 @@ CDFmatching = function(SOURCE,TARGET) {
 #                              (as.vector(SOURCE)[zz] - x_src[nz-1]) +
 #                               y_tar[nz-1]}
    if (SOURCE_IS_RASTER) {
-	  OUTPUT = raster(matrix(y_src,NR,NC),template = rst_template) 
+          OUTPUT = raster(matrix(y_src,NR,NC),template = rst_template)
    } else {
-	  OUTPUT = matrix(y_src,NR,NC)
+          if (is.matrix(SOURCE)) {
+                OUTPUT = matrix(y_src,NR,NC)
+          } else {
+                OUTPUT = y_src
+          }
    }
    return(OUTPUT)
 }
