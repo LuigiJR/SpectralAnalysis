077d9c2cc75eaa98e826841cf8d87bdf0d7898b1
Terrametrica <34562758+LuigiJR@users.noreply.github.com>
2019-10-24 16:35:23 +1100

    CDF matching can be applied to all sorts of data now

--- function_CDFmatching.R
+++ function_CDFmatching.R
@@ -26,33 +26,22 @@ CDFmatching = function(SOURCE,TARGET) {
 
         VALID = which(!is.na(TARGET))  # which of the target data are NAN
 
-#    x_src = sort(as.vector(SOURCE[VALID]))
-#    y_tar = sort(as.vector(TARGET[VALID]))
+    x_src = sort(as.vector(SOURCE[VALID]))
+    y_tar = sort(as.vector(TARGET[VALID]))
 
-#       x_src = as.numeric(quantile(SOURCE[VALID],probs=c(0,0.02,0.1,0.25,0.5,0.75,0.9,0.98,1)))
-        x_src = as.numeric(quantile(SOURCE[VALID],probs=seq(0,1,0.002)))
-#       y_tar = as.numeric(quantile(TARGET[VALID],probs=c(0,0.02,0.1,0.25,0.5,0.75,0.9,0.98,1)))
-        y_tar = as.numeric(quantile(TARGET[VALID],probs=seq(0,1,0.002)))
-
-#    x_unique = unique(x_src)
-#    y_unique = approx(x=x_src,y=y_tar,xout = x_unique,rule=2)$y
-
-#    y_src = approx(x=x_unique,y=y_unique,xout = as.vector(SOURCE),rule=2)$y
         y_src = approx(x=x_src,y=y_tar,xout = as.vector(SOURCE),rule=2)$y
 
     ## to perform linear extrapolation for "xout" beyond x
     ##          Inspiration from https://rdrr.io/cran/Hmisc/src/R/Misc.s
     ##
-#     x_src=x_unique
-#     y_tar=y_unique
-#     rng = range(x_src)
-#     zz  = as.vector(SOURCE) > rng[2]
-#     nz  = length(y_tar)
-#
-#    if (any(zz)) {y_src[zz] = (y_tar[nz] - y_tar[nz-1]) /
-#                              (x_src[nz] - x_src[nz-1]) *
-#                              (as.vector(SOURCE)[zz] - x_src[nz-1]) +
-#                               y_tar[nz-1]}
+     rng = range(x_src)
+     zz  = as.vector(SOURCE) > rng[2]
+     nz  = length(y_tar)
+
+    if (any(zz,na.rm=TRUE)) {y_src[zz] = (y_tar[nz] - y_tar[nz-1]) /
+                              (x_src[nz] - x_src[nz-1]) *
+                              (as.vector(SOURCE)[zz] - x_src[nz-1]) +
+                               y_tar[nz-1]}
    if (SOURCE_IS_RASTER) {
           OUTPUT = raster(matrix(y_src,NR,NC),template = rst_template)
    } else {
